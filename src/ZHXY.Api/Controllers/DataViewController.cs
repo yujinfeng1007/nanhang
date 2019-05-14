6c567d735af969b75bb332761acf78cf61c04069
yss <yss729@hotmail.com>
2019-05-14 18:46:18 +0800

    Signed-off-by: yss <yss729@hotmail.com>

--- src/ZHXY.Api/Controllers/DataViewController.cs
+++ src/ZHXY.Api/Controllers/DataViewController.cs
@@ -15,6 +15,7 @@ namespace ZHXY.Api.Controllers
     /// </summary>
     public class DataViewController : BaseApiController
     {
+
         private DormBuildingService app = new DormBuildingService(new ZhxyRepository());
 
         /// <summary>
@@ -42,6 +43,64 @@ namespace ZHXY.Api.Controllers
                 return Error("0001", e.Message);
             }
 
+        }
+
+        /// <summary>
+        /// 1.2.	大屏绑定楼栋信息
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public IHttpActionResult BindBuildings(GateDto input)
+        {
+            try
+            {
+                app.BindBuildings(input.F_Sn, input.F_BuildingId);
+
+                return Success("Ok");
+
+            }
+            catch (Exception e)
+            {
+                return Error("0001", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 1.3.	大屏解绑楼栋信息
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public IHttpActionResult UnBindBuildings(GateDto input)
+        {
+            try
+            {
+                app.UnBindBuildings(input.F_Sn);
+
+                return Success("Ok");
+
+            }
+            catch (Exception e)
+            {
+                return Error("0001", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 1.5.	获取楼栋学生基本外出在寝信息
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public IHttpActionResult GetBasicDormOutInInfoGroupByBuilld(GateDto input)
+        {
+            try
+            {
+                return Success(app.GetDormOutInInfo(input.F_Sn));
+
+            }
+            catch (Exception e)
+            {
+                return Error("0001", e.Message);
+            }
         }
     }
 }
