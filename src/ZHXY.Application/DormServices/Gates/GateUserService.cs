98505cec92102f6b7a8e0c6e8609dcbddd7a14d0
ben <yibenchen@163.com>
2019-05-14 09:19:03 +0800

    闸机同步

---
+++ src/ZHXY.Application/DormServices/Gates/GateUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZHXY.Application.DormServices.Gates
+{
+    /// <summary>
+    /// 闸机人员信息
+    /// </summary>
+    public class GateUserService
+    {
+    }
+}
