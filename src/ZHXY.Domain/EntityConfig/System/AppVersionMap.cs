871853b4c61687cccee0334680c881d80e943c25
yss <yss729@hotmail.com>
2019-05-17 18:36:48 +0800

    Signed-off-by: yss <yss729@hotmail.com>

---
+++ src/ZHXY.Domain/EntityConfig/System/AppVersionMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace ZHXY.Domain.EntityConfig.System
+{
+    public class AppVersionMap : EntityTypeConfiguration<AppVersion>
+    {
+        public AppVersionMap()
+        {
+            ToTable("Sys_AppVersion");
+            HasKey(t => t.Id);
+        }
+    }
+}
